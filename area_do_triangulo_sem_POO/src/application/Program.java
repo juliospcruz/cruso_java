89d048153af8fcf6bf2bd701065c39a669e37b02
Júlio César Cruz <jc.spc2017@gmail.com>
2026-01-13 00:32:20 -0300

    Orientada_a_objeto_triangulo_area

--- area_do_triangulo_sem_POO/src/application/Program.java
+++ area_do_triangulo_sem_POO/src/application/Program.java
@@ -15,18 +15,17 @@ public class Program {
 		y = new Triangle();
 		
 		System.out.println("Enter the measures of triangle X: ");
-		x.a = sc.nextInt();
-		x.b = sc.nextInt();
-		x.c = sc.nextInt();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 		System.out.println("Enter the measures of triangle Y: ");
-		y.a = sc.nextInt();
-		y.b = sc.nextInt();
-		y.c = sc.nextInt();
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
 		
-		double p = (x.a + x.b + x.c) / 2.0;
-		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-		p = (y.a + y.b + y.c) / 2.0;
-		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+		double areaX = x.area();
+		
+		double areaY = y.area();
 		
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Triangle Y area: %.4f%n", areaY);
